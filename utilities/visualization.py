99c0ec702bb23f72ee5ecea69e783473954a0199
Pinky Gautam <pkgtm1998@gmail.com>
2022-03-22 22:09:08 +0700

    update viz utils

--- utilities/visualization.py
+++ utilities/visualization.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from .data import get_classes
+from .preprocess import label_encode
 
 def plot_feature_importance(feature_importances, algo, columns):
     plt.figure(figsize=(8, 6))
@@ -46,8 +47,16 @@ def plot_numeric_features(data: pd.DataFrame, target_col: str = 'attrition_flag'
         
         for idx, cls in enumerate(data_by_class.keys()):
             label = ' '.join(str(cat_feature).split('_'))
-            sns.histplot(x=cat_feature, data=data_by_class[cls], ax=ax[idx], stat='density', bins=30)
+            sns.histplot(x=cat_feature, data=data_by_class[cls], ax=ax[idx], stat='density', bins=15)
             ax[idx].set_ylabel('Density')
             ax[idx].set_xlabel(label.capitalize())
             ax[idx].set_title('Distribution of {} among {}'.format(label.lower(), cls), fontsize=14)
         plt.show()
+
+def plot_corr_hmap(data: pd.DataFrame, target_col: str):
+    data[target_col] = label_encode(data[target_col], get_classes(data, target_col))
+    corr = data.corr()
+    plt.figure(figsize=(8,6))
+    plt.title('Correlation heatmap of variables')
+    sns.heatmap(corr)
+    plt.show()
