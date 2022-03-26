2e0666a521653c50cb5c22bfc6ed245c94d36a4e
Pinky Gautam <pkgtm1998@gmail.com>
2022-03-26 13:38:47 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 source ${PWD}/venv/bin/activate
 
-# pip3 install pip setuptools wheel
-# python3 setup.py bdist_wheel
-
-# pip3 install --force-reinstal dist/utilities-0.0.0-py3-none-any.whl
-
 mkdir -p ${PWD}/dags/
 
 export AIRFLOW_HOME=~/airflow
