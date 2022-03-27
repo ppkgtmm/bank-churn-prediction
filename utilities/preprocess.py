4e7bac041960ed47a50c21e46037660afc52442e
Pinky Gautam <pkgtm1998@gmail.com>
2022-03-27 18:49:44 +0700

    update preprocess.py

--- utilities/preprocess.py
+++ utilities/preprocess.py
@@ -7,17 +7,21 @@ oh_config = dict(handle_unknown="ignore", sparse=False, dtype=np.int8)
 
 
 def get_feature_preprocessor(X, cat_cols, num_cols, std=True):
-    num_processor = ("standard_scaler", StandardScaler(), num_cols)
-    if not std:
-        num_processor = ("min_max", MinMaxScaler(), num_cols)
+    num_processor = StandardScaler()
+    if std == False:
+        num_processor = MinMaxScaler()
 
     preprocessor = ColumnTransformer(
         [
             ("OH_encoder", OneHotEncoder(**oh_config), cat_cols),
-            num_processor,
+            ("num_preprocessor", num_processor, num_cols),
         ]
     ).fit(X)
-    return preprocessor, preprocessor.transformers_[0][1].get_feature_names_out()
+
+    col_names = (
+        preprocessor.transformers_[0][1].get_feature_names_out().tolist() + num_cols
+    )
+    return preprocessor, col_names
 
 
 def label_encode(y, classes):
