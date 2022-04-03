15dee8c1086c0e6bcf9642a07b57f46042d5c43d
Pinky Gautam <pkgtm1998@gmail.com>
2022-04-03 19:38:09 +0700

    remove unused evaluation code

--- utilities/evaluation.py
+++ utilities/evaluation.py
@@ -1,9 +1,6 @@
 import pandas as pd
 from sklearn.metrics import precision_recall_fscore_support
 
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-
 
 def get_evaluation_report(models: dict, X_test, y_test):
     df = pd.DataFrame(
@@ -15,20 +12,3 @@ def get_evaluation_report(models: dict, X_test, y_test):
         for c, (p, r, f, s) in enumerate(zip(prec, rec, f1, support)):
             df.loc[len(df.index)] = [algo, c, p, r, f, s]
     return df
-
-
-# def plot_confusion_matrix(ax, y_true, y_pred, algo, normalize="true"):
-#     conf_mat = pd.DataFrame(confusion_matrix(y_true, y_pred, normalize=normalize))
-#     #   plt.figure(figsize=(6, 4))
-#     sns.heatmap(conf_mat, annot=True, fmt=".3f", ax=ax)  # plot confusion matrix
-#     plt.title("confusion matrix for {} model".format(algo), fontdict={"size": 16})
-#     plt.ylabel("true class")
-#     plt.xlabel("predicted class")
-#     plt.show()
-
-
-# def print_evaluation_summary(y_true, y_pred, algo):
-
-#     print(classification_report(y_true, y_pred), "\n")
-
-#     plot_confusion_matrix(y_true, y_pred, algo)
