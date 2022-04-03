d70119557684d7403a56e9382a38312d4129f2d5
Pinky Gautam <pkgtm1998@gmail.com>
2022-04-03 16:53:29 +0700

    update and rename modeling function

--- utilities/modeling.py
+++ utilities/modeling.py
@@ -3,29 +3,21 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from pandas.core.frame import DataFrame
-from utilities import print_evaluation_summary, plot_feature_importance
 from constants import seed
 
 algo_map = {
     "Decision Tree": DecisionTreeClassifier,
     "Random Forest": RandomForestClassifier,
     "Logistic Regression": LogisticRegression,
-    "SVC": SVC,
+    "Support Vector Machine": SVC,
 }
 
 
-def model_and_evaluate(X_train, y_train, X_test, y_test, algo):
-    model = algo_map.get(algo)
-
-    if model is None:
-        return model
-
-    model = model(random_state=seed).fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-
-    print_evaluation_summary(y_test, y_pred, algo)
-
-    if hasattr(model, "feature_importances_") and type(X_train) == DataFrame:
-        plot_feature_importance(model.feature_importances_, algo, X_train.columns)
-
-    return model
+def do_moedeling(X_train, y_train):
+    models = []
+    algos = []
+    for algo, cls in algo_map.items():
+        algos.append(algo)
+        model = cls(random_state=seed).fit(X_train, y_train)
+        models.append(model)
+    return algos, models
