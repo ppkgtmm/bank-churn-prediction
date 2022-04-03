c7ec30eaa4fcfbfe2ce12b3be4325b27e86c38bc
Pinky Gautam <pkgtm1998@gmail.com>
2022-04-03 19:37:05 +0700

    update modeling code

--- utilities/modeling.py
+++ utilities/modeling.py
@@ -2,17 +2,17 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-from constants import seed
+from constants import seed, dt_algo, rf_algo, lr_algo, svm_algo
 
 algo_map = {
-    "Decision Tree": DecisionTreeClassifier,
-    "Random Forest": RandomForestClassifier,
-    "Logistic Regression": LogisticRegression,
-    "Support Vector Machine": SVC,
+    dt_algo: DecisionTreeClassifier,
+    rf_algo: RandomForestClassifier,
+    lr_algo: LogisticRegression,
+    svm_algo: SVC,
 }
 
 
-def do_moedeling(X_train, y_train):
+def do_modeling(X_train, y_train):
     models = {}
     for algo, cls in algo_map.items():
         model = cls(random_state=seed, class_weight="balanced").fit(X_train, y_train)
