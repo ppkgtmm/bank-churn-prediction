af0c7dfbedc3883fc7cab3f9f40993684b84383f
Pinky Gautam <pkgtm1998@gmail.com>
2022-05-09 23:17:44 +0700

    remove unused utils function

--- utilities/data.py
+++ utilities/data.py
@@ -1,6 +1,5 @@
 from typing import List
 import pandas as pd
-from constants import json_orient
 
 
 def get_classes(data: pd.DataFrame, column: str):
@@ -14,11 +13,3 @@ def lower_strings(strings: List[str]):
 def read_data(data_path: str, index_col: list[str]):
     data = pd.read_csv(data_path, index_col=index_col)
     return data
-
-
-def serialize_df(df: pd.DataFrame, orient=json_orient):
-    return df.to_json(orient=orient)
-
-
-def deserialize_df(json_str, orient=json_orient):
-    return pd.read_json(str(json_str), orient=orient)
