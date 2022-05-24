8cb441b7e4795017dddb330e4119f3d500435d53
Pinky Gautam <pkgtm1998@gmail.com>
2022-05-25 00:40:32 +0700

    update app version

--- constants.py
+++ constants.py
@@ -47,7 +47,7 @@ svm_algo = "Support Vector Machine"
 report_cmap = "YlGnBu"
 
 app_name = "Churn prediction with machine learning"
-app_version = "1.0.2"
+app_version = "1.0.3"
 best_prep_dir = std_dir
 model_fname = "model.joblib"
 classes = ["Not Churn", "Churn"]
