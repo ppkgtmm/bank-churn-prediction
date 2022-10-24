92d5692bb95a744f55934bc117cdd2dbf40c89b6
Pinky Gautam <pkgtm1998@gmail.com>
2022-10-24 20:13:34 +0700

    update data type

--- ui.py
+++ ui.py
@@ -33,8 +33,8 @@ with col1:
     )
     dependent_count = st.number_input('Dependent Count', min_value=0)
     months_inactive_12_mon = st.number_input('Inactive Months', min_value=0, max_value=12)
-    total_revolving_bal = st.number_input('Total Revolving Balance', min_value=0)
-    total_trans_amt = st.number_input('Total Transaction Amount', min_value=0)
+    total_revolving_bal = st.number_input('Total Revolving Balance', min_value=0.0)
+    total_trans_amt = st.number_input('Total Transaction Amount', min_value=0.0)
 
 with col2:
     education = st.selectbox(
@@ -43,14 +43,14 @@ with col2:
     )
     months_on_book = st.number_input('Months on Book', min_value=0)
     contacts_count_12_mon = st.number_input('Contact Count', min_value=0)
-    total_amt_chng_q4_q1 = st.number_input('Change Amount Q4/Q1', min_value=0)
+    total_amt_chng_q4_q1 = st.number_input('Change Amount Q4/Q1', min_value=0.0)
     total_trans_ct = st.number_input('Total Transaction Count', min_value=0)
 
 with col3:
     age = st.number_input('Age', min_value=0)
     total_relationship_count = st.number_input('Relationship Count', min_value=0)
-    credit_limit = st.number_input('Credit Limit', min_value=0)
-    total_ct_chng_q4_q1 = st.number_input('Change Count Q4/Q1', min_value=0)
+    credit_limit = st.number_input('Credit Limit', min_value=0.0)
+    total_ct_chng_q4_q1 = st.number_input('Change Count Q4/Q1', min_value=0.0)
     avg_utilization_ratio = st.number_input('Average Utilization Ratio', min_value=0.0, max_value=1.0, step=0.01)
 
 st.write('')
