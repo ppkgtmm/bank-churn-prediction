2e5302b613cfa7b7856208b8b2c793f154690741
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-16 23:20:34 +0700

    move/rename files

---
+++ app/api.py
@@ -0,0 +1,47 @@
+import json
+from os.path import join
+import pandas as pd
+from typing import List
+import joblib
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from constants import (
+    app_name,
+    app_version,
+    output_dir,
+    best_prep_dir,
+    preprocessor_fname,
+    model_fname,
+    classes,
+)
+from schemas import PredictionInput, Predictions
+from utilities import decode_label
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+best_prep_path = join(output_dir, best_prep_dir)
+prep_path = join(best_prep_path, preprocessor_fname)
+model_path = join(output_dir, model_fname)
+
+preprocessor = joblib.load(prep_path)
+model = joblib.load(model_path)
+
+
+@app.get("/info")
+def get_app_info():
+    return {"app_name": app_name, "version": app_version}
+
+
+@app.post("/predict")
+def read_item(prediction_inputs: List[PredictionInput]) -> Predictions:
+    df = pd.DataFrame([data.__dict__ for data in prediction_inputs])
+    raw_predictions = model.predict(preprocessor.transform(df))
+    df["prediction"] = decode_label(pd.Series(raw_predictions), classes)
+    return json.loads(df.to_json(orient="records"))
---
+++ app/frontend.py
@@ -0,0 +1,78 @@
+import requests
+import streamlit as st
+import pandas as pd
+
+color_map = {"Churn": "#f54343", "Not Churn": ""}
+
+
+st.markdown(
+    """
+    <style>
+        .stFileUploader {
+            max-width: 46rem;
+        }
+        .block-container {
+            padding-top: 1rem;
+            padding-bottom: 0rem;
+            max-width: 85vw;
+            display: flex;
+            flex-direction: column;
+
+        }
+        .stMarkdown {
+            text-align: center;
+        }
+       .element-container:nth-of-type(2) {
+            max-width: 46rem;
+            margin-left: auto;
+            margin-right: auto
+        }
+    </style>
+""",
+    unsafe_allow_html=True,
+)
+
+
+with st.container():
+    st.write("#### Credit Card Customer Churn Prediction")
+    input_file = st.file_uploader(
+        "Upload input data file", type="csv", accept_multiple_files=False
+    )
+    if input_file:
+        st.write(f"Uploaded {input_file.name}")
+if input_file:
+    used_cols = [
+        "clientnum",
+        "gender",
+        "education_level",
+        "customer_age",
+        "dependent_count",
+        "months_on_book",
+        "total_relationship_count",
+        "months_inactive_12_mon",
+        "contacts_count_12_mon",
+        "credit_limit",
+        "total_revolving_bal",
+        "total_amt_chng_q4_q1",
+        "total_trans_amt",
+        "total_trans_ct",
+        "total_ct_chng_q4_q1",
+        "avg_utilization_ratio",
+    ]
+    df = pd.read_csv(input_file)[used_cols]
+    response = requests.post(
+        "http://localhost:8000/predict",
+        json=df.to_dict(orient="records"),
+        headers={"Content-Type": "application/json"},
+    )
+    df = pd.DataFrame(response.json())
+    df = df[["clientnum", "prediction"] + used_cols[1:]]
+    with st.container():
+        st.dataframe(
+            df.style.apply(
+                lambda x: x.map(color_map).map("background-color: {};".format),
+                axis=1,
+                subset="prediction",
+            ),
+            use_container_width=True,
+        )
